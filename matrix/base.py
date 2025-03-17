5b39f733591b6d6a0ca397f15bfecc817dff6c0c
OraneD <orane.dufour@gmail.com>
2025-03-17 15:37:19 +0100

    base for similarity matrix : map ids = ok, trial/enroll matrix = ok

---
+++ matrix/base.py
@@ -0,0 +1,71 @@
+
+from kiwano.embedding import read_pkl, write_pkl, EmbeddingSet
+import torch
+import pickle
+import random
+from utils import get_avg_tensor
+
+class SimMatrix():
+    def __init__(self, enroll_path, trial_path,L,seed):
+        self.enroll_embeddings = read_pkl(f"pkl:{enroll_path}")
+        with open(trial_path, "rb") as handle :
+            self.trial_embeddings = pickle.load(handle)
+        self.L = L
+        self.seed = seed
+        random.seed(seed)
+        self.trial_ids = self.__map_trial_ids()
+        self.enroll_ids = self.__map_enroll_ids()
+        self.trial_matrix = self.__get_trial_matrix()
+        self.enroll_matrix = self.__get_enroll_matrix()
+
+    def __map_trial_ids(self):
+        return {spk_id : idx for idx, spk_id in enumerate(list(self.trial_embeddings.keys()))}
+    
+    def __map_enroll_ids(self):
+        enroll_ids = {spk_id: idx for idx, spk_id in enumerate(self.trial_embeddings.keys())}
+        next_id = len(self.trial_embeddings)
+        for spk_id in self.enroll_embeddings.h.keys():
+            if spk_id not in self.trial_embeddings:
+                enroll_ids[spk_id] = next_id
+                next_id += 1
+        return enroll_ids
+
+
+    def __get_enroll_matrix(self):
+            embeddings_list = [torch.tensor(self.enroll_embeddings.h[spk_id]) for spk_id in sorted(self.enroll_ids, key=self.enroll_ids.get)]
+            return torch.stack(embeddings_list) 
+
+    def __get_trial_matrix(self):
+
+        if self.L == 1 :
+            embeddings_list = [torch.tensor(random.sample(self.trial_embeddings[spk_id], 1)[0]) 
+                                for spk_id in  sorted(self.trial_ids, key=self.trial_ids.get)]
+            return torch.stack(embeddings_list)
+        elif self.L > 1 :
+            embeddings_list = [torch.tensor(self.compute_random_avg(self.trial_embeddings[spk_id])) 
+                                for spk_id in  sorted(self.trial_ids, key=self.trial_ids.get)]
+            return torch.stack(embeddings_list)
+
+
+    def compute_random_avg(self, lst_tensor):
+        if len(lst_tensor) >= self.L: # If there is more embeddings then needed for 1 speaker, we randomly select L embeddings
+         selected_embeddings = random.sample(lst_tensor, self.L)
+         return get_avg_tensor(selected_embeddings)
+        elif len(lst_tensor) == 1 :   # If there's only 1 embedding for the speaker, we return the one embbeding (it's the case for 1 speaker in whole dataset)
+            return lst_tensor[0]
+        else :                        # If there's not enough embedding for the speaker, we compute the average embedding with all embeddings from the speaker anyway
+         return get_avg_tensor(lst_tensor) 
+
+
+
+                      
+
+matrix = SimMatrix("../data/embs_avg_cv11-A_Vox2_libri-54_anon_B5.pkl", "../data/spk2embs_cv11-B_Vox2_libri-54_anon_B5.pkl",10, 42)
+print(matrix.trial_matrix.shape)
+for i, key in enumerate(matrix.trial_ids.keys()):
+    if matrix.trial_ids[key] == matrix.enroll_ids[key] :
+        print(key)
+        print(f"{matrix.trial_ids[key]}, {matrix.enroll_ids[key]}, ok")
+    else:
+        print(f"not ok at index {i}")
+
