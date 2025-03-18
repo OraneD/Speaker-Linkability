1fb0250e18bc646a333982971215eab291a8c5ca
OraneD <orane.dufour@gmail.com>
2025-03-18 18:12:22 +0100

    modify logger

--- utils/logger.py
+++ utils/logger.py
@@ -3,18 +3,28 @@ import datetime
 from datetime import datetime
 import os
 
-def setup_logger(log_dir, matrix_type):
-        os.makedirs(log_dir, exist_ok=True)
-        now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        log_file = os.path.join(log_dir, f"{now}.log")
+import logging
+import os
+from datetime import datetime
 
-        logging.basiConfig(level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        handlers=[
-            logging.FileHandler(log_file),
-            logging.StreamHandler()
-            ]
-        )
-        return logging.getLogger(matrix_type)
+def setup_logger(log_dir="logs", name="SimMatrix"):
+    os.makedirs(log_dir, exist_ok=True)
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_file = os.path.join(log_dir, f"{now}_{name}.log")
+
+    logger = logging.getLogger(name)  
+    if not logger.hasHandlers(): 
+        handler = logging.FileHandler(log_file)
+        handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+        stream_handler = logging.StreamHandler()  # Affiche les logs dans la console
+        stream_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+        logger.addHandler(handler)
+        logger.addHandler(stream_handler)
+        logger.setLevel(logging.INFO)
+
+    return logger
+
 
     
