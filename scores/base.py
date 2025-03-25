687829597ec4c9b50d8b96508d4e59880215bab6
OraneD <orane.dufour@gmail.com>
2025-03-25 14:01:17 +0100

    avg linkability per speaker

--- scores/base.py
+++ scores/base.py
@@ -1,7 +1,14 @@
 import pickle
 import glob
+import os
 from tqdm import tqdm
 from utils import setup_logger
+
+"""
+Takes a directory containing dictionaries matrix scores and computes :
+        - The overall linkability across all seeds and N enrollment speaker for the given matrix
+        - The overall linkability across all seends and N enrollment speaker for all trial speakers of the given matrix
+"""
 
 class Scores():
     def __init__(self, dirpath):
@@ -11,6 +18,7 @@ class Scores():
         self.logger.info(f"Computing Linkability for Matrix {self.dirpath}")
         self.N_enrolls = [22024, 11012, 5506, 2753, 1376, 688, 344, 172, 86, 43, 21]
         self.all_scores = self.__get_all_scores()
+        self.speaker_scores = self.__get_speaker_scores()
 
 
     def __get_all_scores(self):
@@ -18,13 +26,31 @@ class Scores():
         for N in self.N_enrolls :
             self.logger.info(f"Computing overall linkability for {N} enroll spk ...")
             files = glob.glob(f"{self.dirpath}/*{N}*.pkl")
-            for file in tqdm(files, total=len(files) , desc=f"Processing files for {N} enroll spk..."):
+            for file in tqdm(files, total=len(files)):
                 with open(file, "rb") as handle :
                      score_dictionary = pickle.load(handle)
                      all_results[N] = self.compute_avg_score(score_dictionary)
         return all_results
-
     
+    def __get_speaker_scores(self):
+        os.makedirs("experiment/scores", exist_ok=True)
+        speaker_scores = {}
+        files = glob.glob(f"{self.dirpath}/*.pkl")
+        self.logger.info(f"Computing speaker scores for matrix {self.dirpath}...")
+        for file in tqdm(files, total=len(files), desc=f"Retrieving scores for speakers..."):
+            with open(file, "rb") as handle :
+                score_dictionary = pickle.load(handle)
+                for speaker,value in score_dictionary.items():
+                    if speaker not in speaker_scores:
+                        speaker_scores[speaker] = []
+                    speaker_scores[speaker].append(value[0])
+        self.logger.info(f"Speaker score computed for {len(speaker_scores)} speakers")
+        avg_speaker_scores ={speaker:sum(value)/len(value) for speaker, value in speaker_scores.items()}
+        with open(f"experiment/scores/speaker_scores_{self.dirpath.split('/')[-1]}.pkl", "wb") as handle :
+            pickle.dump(avg_speaker_scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        return avg_speaker_scores
+
+
     @staticmethod
     def compute_avg_score(score_dictionary):
         all_score = [x[0] for x in list(score_dictionary.values())]
